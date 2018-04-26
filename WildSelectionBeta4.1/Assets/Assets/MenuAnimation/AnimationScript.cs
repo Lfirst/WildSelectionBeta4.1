90a1c8f1b1cf1209b2bd0836610cc8d032bdcc9b
Lfirst <lulu101m@yahoo.fr>
2018-04-26 14:51:47 +0200

    Option Graphique

---
+++ WildSelectionBeta4.1/Assets/Assets/MenuAnimation/AnimationScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationScript : MonoBehaviour {
+
+	public Animator anim;
+	int Breath = Animator.StringToHash("Mutant Breathing Idle");
+	int Walk = Animator.StringToHash("Orc Walk");
+	int turn = Animator.StringToHash("Left Turn 45");
+
+
+	void Start ()
+	{
+		anim = GetComponent<Animator>();
+	}
+
+
+	void Update ()
+	{
+		anim.SetTrigger (Breath);
+	}
+}
